00e36c5a3f871f834683179dd77c5af48b1a3405
Robert Hargreaves <kiyouta@gmail.com>
2025-03-13 15:38:46 +0000

    Remove old x86_64 code. We're running ARM64 E2E now

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,5 @@
 set -euo pipefail
 
 rm -f bootstrap api.zip || true
-
-if [ "$(uname -m)" = "arm64" ]; then
-    ARCH="arm64"
-else
-    ARCH="amd64"
-fi
-GOARCH="${GOARCH:-$ARCH}"
-
-GOOS=linux go build -o bootstrap main.go
+GOOS=linux GOARCH=arm64 go build -o bootstrap main.go
 zip api.zip bootstrap
