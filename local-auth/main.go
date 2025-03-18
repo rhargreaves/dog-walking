6b792a8711aec828c248c66ee09dee286fe7584e
Robert Hargreaves <kiyouta@gmail.com>
2025-03-18 19:21:52 +0000

    More local-auth refactoring

--- local-auth/main.go
+++ local-auth/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -11,44 +12,10 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func authorisedResponse(principalID string, methodArn string, email string, groups []string) events.APIGatewayV2CustomAuthorizerIAMPolicyResponse {
-	return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{
-		PrincipalID: principalID,
-		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
-			Version: "2012-10-17",
-			Statement: []events.IAMPolicyStatement{
-				{
-					Action:   []string{"execute-api:Invoke"},
-					Effect:   "Allow",
-					Resource: []string{methodArn},
-				},
-			},
-		},
-		Context: map[string]interface{}{
-			"userId": principalID,
-			"email":  email,
-			"groups": groups,
-		},
-	}
-}
-
-func errorResponse(errorMessage string, methodArn string) events.APIGatewayV2CustomAuthorizerIAMPolicyResponse {
-	return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{
-		PrincipalID: "",
-		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
-			Version: "2012-10-17",
-			Statement: []events.IAMPolicyStatement{
-				{
-					Action:   []string{"execute-api:Invoke"},
-					Effect:   "Deny",
-					Resource: []string{methodArn},
-				},
-			},
-		},
-		Context: map[string]interface{}{
-			"error": errorMessage,
-		},
-	}
+type AuthorisedClaims struct {
+	User   string
+	Email  string
+	Groups []string
 }
 
 func handleRequest(ctx context.Context,
@@ -57,51 +24,60 @@ func handleRequest(ctx context.Context,
 		return errorResponse("no MethodArn provided", ""), nil
 	}
 
-	jwtSecret := os.Getenv("LOCAL_JWT_SECRET")
-	if jwtSecret == "" {
-		return errorResponse("environment variable LOCAL_JWT_SECRET missing", event.MethodArn), nil
-	}
-
-	tokenString := event.AuthorizationToken
-	if tokenString == "" {
-		return errorResponse("no AuthorizationToken provided", event.MethodArn), nil
-	}
-	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte(jwtSecret), nil
-	})
+	authorisedClaims, err := authorise(event.AuthorizationToken, os.Getenv("LOCAL_JWT_SECRET"))
 	if err != nil {
 		return errorResponse(err.Error(), event.MethodArn), nil
 	}
 
+	return authorisedResponse(authorisedClaims.User,
+		event.MethodArn,
+		authorisedClaims.Email,
+		authorisedClaims.Groups), nil
+}
+
+func authorise(authorisationToken string, jwtSecret string) (AuthorisedClaims, error) {
+	if jwtSecret == "" {
+		return AuthorisedClaims{}, errors.New("JWT secret missing")
+	}
+	if authorisationToken == "" {
+		return AuthorisedClaims{}, errors.New("no AuthorizationToken provided")
+	}
+	tokenString := strings.TrimPrefix(authorisationToken, "Bearer ")
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtSecret), nil
+	})
+	if err != nil {
+		return AuthorisedClaims{}, err
+	}
+
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok || !token.Valid {
-		return errorResponse("token is invalid", event.MethodArn), nil
+		return AuthorisedClaims{}, errors.New("token is invalid")
 	}
 
 	fmt.Println("token OK: claims:", claims)
 
 	sub, ok := claims["sub"].(string)
 	if !ok {
-		return errorResponse("token has no sub claim", event.MethodArn), nil
+		return AuthorisedClaims{}, errors.New("token has no sub claim")
 	}
 
 	email, ok := claims["email"].(string)
 	if !ok {
-		return errorResponse("token has no email claim", event.MethodArn), nil
+		return AuthorisedClaims{}, errors.New("token has no email claim")
 	}
 
 	groups, ok := claims["cognito:groups"].([]interface{})
 	if !ok {
-		return errorResponse("token has no cognito:groups claim", event.MethodArn), nil
+		return AuthorisedClaims{}, errors.New("token has no cognito:groups claim")
 	}
 
-	return authorisedResponse(
-		sub,
-		event.MethodArn,
-		email,
-		convertToStringSlice(groups),
-	), nil
+	return AuthorisedClaims{
+		User:   sub,
+		Email:  email,
+		Groups: convertToStringSlice(groups),
+	}, nil
+
 }
 
 func convertToStringSlice(slice []interface{}) []string {
---
+++ local-auth/responses.go
@@ -0,0 +1,37 @@
+package main
+
+import "github.com/aws/aws-lambda-go/events"
+
+func response(principalID string, methodArn string, context map[string]interface{},
+	effect string) events.APIGatewayV2CustomAuthorizerIAMPolicyResponse {
+	return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{
+		PrincipalID: principalID,
+		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
+			Version: "2012-10-17",
+			Statement: []events.IAMPolicyStatement{
+				{
+					Action:   []string{"execute-api:Invoke"},
+					Effect:   effect,
+					Resource: []string{methodArn},
+				},
+			},
+		},
+		Context: context,
+	}
+}
+
+func errorResponse(errorMessage string,
+	methodArn string) events.APIGatewayV2CustomAuthorizerIAMPolicyResponse {
+	return response("", methodArn, map[string]interface{}{
+		"error": errorMessage,
+	}, "Deny")
+}
+
+func authorisedResponse(principalID string, methodArn string, email string,
+	groups []string) events.APIGatewayV2CustomAuthorizerIAMPolicyResponse {
+	return response(principalID, methodArn, map[string]interface{}{
+		"userId": principalID,
+		"email":  email,
+		"groups": groups,
+	}, "Allow")
+}
