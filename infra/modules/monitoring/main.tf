33e5e9a79b7be8ea94d066309d1a1cceb3c4b339
Robert Hargreaves <kiyouta@gmail.com>
2025-03-20 15:57:26 +0000

    Add per-endpoint error rate alarms

--- infra/modules/monitoring/main.tf
+++ infra/modules/monitoring/main.tf
@@ -108,6 +108,69 @@ resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "endpoint_error_rate" {
+  for_each = {
+    "create-dog" = { path = "/dogs", method = "POST" },
+    "list-dogs" = { path = "/dogs", method = "GET" },
+    "get-dog" = { path = "/dogs/{id}", method = "GET" },
+    "update-dog" = { path = "/dogs/{id}", method = "PUT" },
+    "delete-dog" = { path = "/dogs/{id}", method = "DELETE" },
+    "upload-dog-photo" = { path = "/dogs/{id}/photo", method = "PUT" },
+    "detect-dog-breed" = { path = "/dogs/{id}/photo/detect-breed", method = "POST" }
+  }
+
+  alarm_name                = "${var.environment}-${var.application_name}-${each.key}-error-rate"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 12
+  threshold                 = 1
+  alarm_description         = "Error rate > 1% for ${each.key}"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+  insufficient_data_actions = []
+
+  metric_query {
+    id          = "rate"
+    expression  = "(errors*100)/requests"
+    label       = "Error Rate"
+    return_data = "true"
+  }
+
+  metric_query {
+    id = "requests"
+
+    metric {
+      metric_name = "Count"
+      namespace   = "AWS/ApiGateway"
+      period      = 300
+      stat        = "Sum"
+      unit        = "Count"
+
+      dimensions = {
+        ApiId = var.api_id
+        Resource = each.value.path
+        Method = each.value.method
+      }
+    }
+  }
+
+  metric_query {
+    id = "errors"
+
+    metric {
+      metric_name = "5XXError"
+      namespace   = "AWS/ApiGateway"
+      period      = 300
+      stat        = "Sum"
+      unit        = "Count"
+
+      dimensions = {
+        ApiId = var.api_id
+        Resource = each.value.path
+        Method = each.value.method
+      }
+    }
+  }
+}
+
 resource "aws_sns_topic" "alerts" {
   name = "${var.environment}-${var.application_name}-alerts"
 }
