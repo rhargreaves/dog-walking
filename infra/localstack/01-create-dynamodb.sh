7e1d2bccf14f50f64eb43d61ec4cde6090226841
Robert Hargreaves <kiyouta@gmail.com>
2025-03-21 13:39:11 +0000

    Attempt to fix local stack failing on GH actions

--- infra/localstack/01-create-dynamodb.sh
+++ infra/localstack/01-create-dynamodb.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 TABLE_NAME=local-dogs
+AWS_REGION=eu-west-1
+
 awslocal dynamodb create-table \
     --table-name ${TABLE_NAME} \
     --attribute-definitions AttributeName=id,AttributeType=S \
     --key-schema AttributeName=id,KeyType=HASH \
-    --billing-mode PAY_PER_REQUEST
+    --billing-mode PAY_PER_REQUEST \
+    --region ${AWS_REGION}
 
 echo "DynamoDB table '${TABLE_NAME}' created."
