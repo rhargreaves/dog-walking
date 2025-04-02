d3ccdf7952ef3cb13c3b1eafddbee6aa0e439a8b
Robert Hargreaves <kiyouta@gmail.com>
2025-04-02 16:40:38 +0100

    Deploy photo moderation to PROD

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -34,7 +34,7 @@ module "api" {
   s3_access_policy_arn       = module.images.s3_access_policy_arn
   dogs_table_name            = module.data.dogs_table_name
   dog_images_bucket          = module.images.bucket_name
-  bootstrap_path             = var.bootstrap_path
+  bootstrap_path             = var.api_bootstrap_path
   cognito_client_ids         = module.auth.cognito_client_ids
   cognito_user_pool_id       = module.auth.cognito_user_pool_id
   cors_allowed_origin        = var.cors_allowed_origin
@@ -56,4 +56,16 @@ module "monitoring" {
   environment          = var.environment
   api_id               = module.api.api_id
   lambda_function_name = module.api.lambda_function_name
+}
+
+module "photo_moderation" {
+  source = "../../modules/photo-moderation"
+
+  environment                    = var.environment
+  dynamodb_access_policy_arn     = module.data.dynamodb_access_policy_arn
+  s3_access_policy_arn           = module.images.s3_access_policy_arn
+  dogs_table_name                = module.data.dogs_table_name
+  dog_images_bucket              = module.images.bucket_name
+  pending_dog_images_bucket_name = module.images.pending_dog_images_bucket_name
+  bootstrap_path                 = var.photo_moderation_bootstrap_path
 }
